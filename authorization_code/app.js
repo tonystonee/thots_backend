989bc43c1db88b8855644aaa02158e985b713373
Anthony Dionise <anthonydionise@Anthonys-MacBook-Pro.local>
2020-11-25 19:35:32 -0500

    idk

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -139,13 +139,12 @@ app.get('/refresh_token', function(req, res) {
   };
 
   request.post(authOptions, function(error, response, body) {
-    console.log(response.statusCode)
     if (!error && response.statusCode === 200) {
       var access_token = body.access_token;
       res.send({
         'access_token': access_token
       });
-    }
+    } 
   });
 });
 
